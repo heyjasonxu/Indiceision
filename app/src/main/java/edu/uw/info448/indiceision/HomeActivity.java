90ca59af4dd0d964f91df902f8cac97a698cf637
Jason <heyjasonxu@github.com>
2017-12-04 14:07:12 -0800

    working maps

--- app/src/main/java/edu/uw/info448/indiceision/HomeActivity.java
+++ app/src/main/java/edu/uw/info448/indiceision/HomeActivity.java
@@ -1,5 +1,6 @@
 package edu.uw.info448.indiceision;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent intent = new Intent(this, LocationDetails.class);
+        startActivity(intent);
     }
 }
