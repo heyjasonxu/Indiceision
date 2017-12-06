0651d0da0bb4c479034db00938338e5dbd860c19
Jason <heyjasonxu@github.com>
2017-12-05 23:57:45 -0800

    final

--- app/src/main/java/edu/uw/info448/indiceision/Reviews.java
+++ app/src/main/java/edu/uw/info448/indiceision/Reviews.java
@@ -35,14 +35,14 @@ public class Reviews extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         mListView = (ListView) findViewById(R.id.reviews_list_view);
 // 1
